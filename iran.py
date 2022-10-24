ed49aee95cb5c430f8e908ffdc25f323a08407fc
Betisa <betisa@Air-von-Betisa.fritz.box>
2022-10-24 11:32:20 +0200

    first commit

---
+++ iran.py
@@ -0,0 +1,34 @@
+from xml.sax.handler import all_features
+import streamlit as st
+import pandas as pd
+import numpy as np
+#from streamlit_chat import message
+
+df = pd.read_csv("/Users/betisa/Desktop/iran/deputies.csv", sep=";")
+
+st.header(":mailbox: Schreibe deinen Abgeordneten")
+
+st.write("Seit Wochen weht im Iran ein Wind der Veränderungen. Und seit Wochen lehnen sich immer mehr gesellschaftliche Schichten gegen das Unrechtsregime im Iran.")
+
+@st.cache(suppress_st_warning= True, allow_output_mutation=True)
+def get_info(plz):
+    info = df[["Abgeordnete", "E-Mail", "Partei", "Wahlkreis"]].loc[df.PLZ.str.contains(plz)]
+    #info = df[["Abgeordnete", "E-Mail", "Partei", "Wahlkreis"]].loc[df.PLZ.str.contains(plz)].sort_values(by=["Wahlkreis"])
+    info = info.set_index("Abgeordnete").sort_values(by=["Wahlkreis"])
+    return info
+
+with st.form("mail"):
+    name = st.text_input("Vollständiger Name")
+    email = st.text_input("E-Mail Adresse")
+    plz = st.text_input("Deine Postleitzahl")
+    
+    submit = st.form_submit_button("Weiter")
+    if submit:
+        df = get_info(plz)
+        st.table(df)
+        email_list = df["E-Mail"]
+        contact = st.multiselect("Möchtest du gewissen Abgeordneten NICHT schreiben? Diese kannst du hier entfernen:", email_list, default=email_list)
+        st.write("Wir empfehlen grundsätzlich ALLEN Abgeordneten (parteiübergreifend) zu schreiben.")
+
+st.text_area("Deine E-Mail", value="""Sehr geehrte/r Abgeordnete/r, \nseit Wochen weht im Iran ein Wind der Veränderungen. Und seit Wochen lehnen sich immer mehr gesellschaftliche Schichten gegen das Unrechtsregime im Iran.
+             """)
